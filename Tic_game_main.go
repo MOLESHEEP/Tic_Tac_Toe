a3c06b28ac3059abbaa3ac437a6059275ab8e9a5
Aoto Onioka <nansskao1431h@gmail.com>
2023-07-05 02:15:31 +0000

    Aoto Onioka

--- Tic_game_main.go
+++ Tic_game_main.go
@@ -6,20 +6,56 @@ import (
 
 const (
 	boardSize = 3
+	emptyCell = "."
+	playerX   = "X"
+	playerO   = "O"
 )
 
 var board [boardSize][boardSize]string
 
+func initialBoard() {
+	for i := 0; i < boardSize; i++ {
+		for j := 0; j < boardSize; j++ {
+			board[i][j] = emptyCell
+		}
+	}
+}
+
 func printBoard() {
 	for i := 0; i < boardSize; i++ {
 		for j := 0; j < boardSize; j++ {
-			fmt.Print(board[i][j], ".")
+			fmt.Print(board[i][j])
 		}
 		fmt.Print("\n")
 	}
 }
 
-func main() {
-	printBoard()
+func isBoardFull() bool {
+	for i := 0; i < boardSize; i++ {
+		for j := 0; j < boardSize; j++ {
+			if board[i][j] == emptyCell {
+				return false
+			}
+		}
+	}
+	return true
+}
 
+func main() {
+	initialBoard()
+	var row, col int
+	for {
+		fmt.Print("Input (x,y) ")
+		fmt.Scan(&row, &col)
+
+		if row < 0 || row >= boardSize || col < 0 || col >= boardSize {
+			fmt.Println("無効な行または列です。もう一度入力してください。")
+		}
+		board[row][col] = playerO
+		printBoard()
+
+		if isBoardFull() {
+			break
+		}
+	}
 }
